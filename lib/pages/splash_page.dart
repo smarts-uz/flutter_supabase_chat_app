0c80167bfdd8bb425544d9a34f7d35b0f98b6744
JasurbekErgashev <jergashev633@gmail.com>
2023-09-14 14:19:09 +0500

    redirected users depending on auth state using splash page

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:chat_app/pages/chat_page.dart';
+import 'package:chat_app/pages/register_page.dart';
+import 'package:chat_app/utils/constants.dart';
+
+/// Page to redirect users to the appropriate page depending on the initial auth state
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  SplashPageState createState() => SplashPageState();
+}
+
+class SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _redirect();
+  }
+
+  Future<void> _redirect() async {
+    // await for for the widget to mount
+    await Future.delayed(Duration.zero);
+
+    final session = supabase.auth.currentSession;
+    if (session == null) {
+      if (context.mounted) {
+        Navigator.of(context).pushAndRemoveUntil(
+          RegisterPage.route(),
+          (route) => false,
+        );
+      }
+    } else {
+      if (context.mounted) {
+        Navigator.of(context).pushAndRemoveUntil(
+          ChatPage.route(),
+          (route) => false,
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: preloader);
+  }
+}
