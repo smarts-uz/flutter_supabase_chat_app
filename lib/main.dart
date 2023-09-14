70f3c206a4b9ef21ca1899fdc8f361120e6ede35
JasurbekErgashev <jergashev633@gmail.com>
2023-09-14 15:42:37 +0500

    added profiles cubit and registered it in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,33 @@
-import 'package:chat_app/utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:chat_app/cubits/profiles/profiles_cubit.dart';
+import 'package:chat_app/utils/constants.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:chat_app/pages/splash_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Supabase.initialize(
     url: 'https://mytqgqtxrpiadytjpbqw.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15dHFncXR4cnBpYWR5dGpwYnF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ2ODE2OTEsImV4cCI6MjAxMDI1NzY5MX0.7EBbgklTYHVmsrwPDODOQMLPHTv5-u7LDipWORvzgNU',
+    authCallbackUrlHostname: 'login',
   );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'My Chat App',
-      theme: appTheme,
-      home: const SplashPage(),
+    return BlocProvider<ProfilesCubit>(
+      create: (context) => ProfilesCubit(),
+      child: MaterialApp(
+        title: 'SupaChat',
+        debugShowCheckedModeBanner: false,
+        theme: appTheme,
+        home: const SplashPage(),
+      ),
     );
   }
 }
